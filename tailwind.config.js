0182fa5787b4fbd058b2377db7a02a931437fab2
Makedonka Tochevska <makedonkatochevska@gmail.com>
2025-06-08 14:22:23 +0200

    starter files added

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // catch ALL html/js files inside src
+  safelist: ["bg-cream"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
